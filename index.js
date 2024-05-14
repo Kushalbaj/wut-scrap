97495cec7d7bfea3bf366f3a15ff70bddb8adf8a
Kushal Bajracharya <bajrakus20@gmail.com>
2024-05-14 07:35:31 +0545

    chore: Update puppeteer usage and add login functionality
    This commit updates the usage of puppeteer in the code and adds functionality to perform a login on the specified website. The code now launches puppeteer with the `headless` option set to `false`, allowing the browser to be visible during execution. It then navigates to the login page, fills in the username and password fields, and clicks the login button. Finally, it waits for the navigation to complete.
    
    The changes also include updating the `start` script in the `package.json` file to run the `index.js` file using Node.
    
    Closes #123

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function run() {
-  const browser = await puppeteer.launch();
+(async () => {
+  try{
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto("http://tns.uswut.com/main/index.do");
 
-  await browser.close();
-}
+  //   await page.screenshot({ path: "example.png", fullPage: true });
+  //   await page.pdf({ path: "example.pdf", format: "A4" });
+
+  //   const html = await page.content();
+  //   console.log(html);
+
+  //   const title = page.evaluate(() => document.title);
+  //   console.log(await title);
+
+  //    const text = await page.evaluate(() => document.body.innerText);
+  //    console.log(text);
+
+  // const links = await page.evaluate(() => Array.from(document.body.querySelectorAll('a'), (e) => e.href));
+  // console.log(links);
+
+  await page.type('#pUsrId', 'ADAS'); 
+  await page.type('#pUsrPwd', 'adaexpress1'); 
+
+  await page.click('img[src="/resource/img/img_input_login.gif"]');
+
+  await page.click('a[href="javascript:getContentUrl(&quot;/uiArp05Action/openScreen.do&quot;)"]');
+  
+  
+  await page.waitForNavigation();
+
+  // await browser.close();
+  }catch(error){
+    console.error('An error occurred: ', error);
+  }
+})();
